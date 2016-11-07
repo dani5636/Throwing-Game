235641cc1daad2e4e3b9441cbcdc6ed1fb49c208
dani5636 <dani5636@easv365.dk>
2016-11-07 13:08:57 +0100

    V0.3 - The Duckening
    The duck pictures has been added to the game.

--- MyWorld.java
+++ MyWorld.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public abstract class MyWorld extends World
 {
 
     /**
@@ -16,6 +16,6 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 600, 1); 
+        super(1600, 600, 1); 
     }
 }
---
+++ lvl1.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class lvl1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class lvl1 extends MyWorld
+{
+
+    /**
+     * Constructor for objects of class lvl1.
+     * 
+     */
+    public lvl1()
+    {
+    }
+}
